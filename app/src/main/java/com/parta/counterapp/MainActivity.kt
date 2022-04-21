9db9e77897163234b0d498cccf0fabeafa206ecc
mksd0398 <mayank0398@gmail.com>
2022-04-21 21:10:22 +0530

    First Blood

---
+++ app/src/main/java/com/parta/counterapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.parta.counterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mainHandler = Handler(Looper.getMainLooper())
+
+        var count = 0
+
+        start.setOnClickListener{
+            mainHandler.post(object : Runnable {
+                override fun run() {
+
+                    counter.text = count.toString()
+                    count++
+                    mainHandler.postDelayed(this, 1000) //1 second delay for demo can increase or decrease
+
+                }
+            })
+        }
+
+        stop.setOnClickListener { mainHandler.removeCallbacksAndMessages(null) }
+
+
+    }
+}
